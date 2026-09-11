8f4496fcd36e8f714c11d03c2598af8fa2b221a8
Ash Connolly <ashconnollydigital@gmail.com>
2026-09-11 16:40:47 +0100

    env vars updated

--- src/server/routes/example/example-api.js
+++ src/server/routes/example/example-api.js
@@ -9,7 +9,7 @@ export class BackendError extends Error {
 }
 
 function backendUrl(path) {
-  return new URL(path, config.get('wasteBatteriesRegBackendUrl'))
+  return new URL(path, config.get('wasteBatteriesObligationsBeUrl'))
 }
 
 function backendHeaders(accessToken, extraHeaders = {}) {
